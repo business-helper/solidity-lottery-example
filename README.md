77b1d564a24cd1e8871db70ae671d5d9347c59a1
Bi Wu <alerk_star@outlook.com>
2022-03-19 16:06:49 +0800

    init;

---
+++ README.md
@@ -0,0 +1,4 @@
+
+1. users can enter lottery with ETH based on a USD fee
+2. And admin will choose when the lottery is over
+3. The lottery will select a random winner.
---
+++ contracts/Lottery.sol
@@ -0,0 +1,14 @@
+
+pragma solidity ^0.6.6;
+
+contract Lottery {
+
+  function enter() public {}
+
+  function getEntranceFee() public{}
+
+  function startLottery() public{}
+
+  function endLottery() public {}
+
+}

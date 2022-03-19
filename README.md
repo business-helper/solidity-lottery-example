406697080fc5e58d8bce0b3741e8eb43a2a24d3d
Bi Wu <alerk_star@outlook.com>
2022-03-19 16:50:52 +0800

    added openzeppelin;

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 1. users can enter lottery with ETH based on a USD fee
 2. And admin will choose when the lottery is over
 3. The lottery will select a random winner.
+
+## How do we want to test this?
+
+- `mainnet-fork`
+- `development` with mocks
+- `testnet`
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,13 +1,54 @@
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
 
-contract Lottery {
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-  function enter() public {}
+contract Lottery is Ownable {
+  using SafeMathChainlink for uint256;
 
-  function getEntranceFee() public{}
+  address payable[] public players;
+  uint256 public usdEntryFee;
+  AggregatorV3Interface internal ethUsdPriceFeed;
+  enum LOTTERY_STATE {
+    OPEN,
+    CLOSED,
+    CALCULATING_WINNER
+  }
+  LOTTERY_STATE public lottery_state;
 
-  function startLottery() public{}
+  constructor(address _priceFeedAddress) public {
+    usdEntryFee = 50 * (10 ** 18);
+    ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    lottery_state = LOTTERY_STATE.CLOSED;
+  }
+
+  function enter() public payable {
+    // $50 minimum
+    require(lottery_state == LOTTERY_STATE.OPEN);
+    require(msg.value > getEntranceFee(), "Not enough ETH!");
+    players.push(msg.sender);
+  }
+
+  function getEntranceFee() public view returns (uint256) {
+    (
+        /*uint80 roundID*/,
+        int price,
+        /*uint startedAt*/,
+        /*uint timeStamp*/,
+        /*uint80 answeredInRound*/
+    ) = ethUsdPriceFeed.latestRoundData();
+    uint256 adjustedPrice = uint256(price) * 10 ** 10; // 18 decimals.
+
+    uint256 costToEnter = (usdEntryFee * 10 ** 18) / adjustedPrice;
+    return costToEnter;
+  }
+
+  function startLottery() public onlyOwner {
+    require(lottery_state == LOTTERY_STATE.CLOSED, "Can't start a new lottery yet!");
+    lottery_state = LOTTERY_STATE.OPEN;
+  }
 
   function endLottery() public {}
 
